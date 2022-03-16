266579eb6d13598f66610808e3c839a35108c34c
Kalemeera Collins <kalemeeracollins@outlook.com>
2022-03-16 22:49:12 +0300

    First commit

---
+++ src/requests.js
@@ -0,0 +1,17 @@
+// API KEY
+const API_KEY = "96c0d4ae4c61d740de7427a252eb54e7";
+
+// Object for various requests for movies
+const request = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+// Exporting request object. Making it available
+export default request;
